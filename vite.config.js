36dc153a8d03f02d7ec45e05f07856918e607191
Alan250812GPR <alan.2500gpr@gmail.com>
2024-08-25 20:14:02 -0600

    Antes de Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { config } from 'dotenv';
-import path from "path";
 
-// Load environment variables from .env file
 config();
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
