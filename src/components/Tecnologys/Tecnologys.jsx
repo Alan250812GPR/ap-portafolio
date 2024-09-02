e429607ea3f5483130dfc68b2a75c0b8225e0a48
Alan250812GPR <alan.2500gpr@gmail.com>
2024-09-01 23:58:01 -0600

    Galeria de fotos con tecnologias en avance

--- src/components/Tecnologys/Tecnologys.jsx
+++ src/components/Tecnologys/Tecnologys.jsx
@@ -1,5 +1,130 @@
+import { Gallery } from "react-grid-gallery";
+
+const images = [
+   {
+      src: "../../../public/tec/Abap.jpeg",
+      width: 320,
+      height: 212,
+   },
+   {
+    src: "../../../public/tec/Angular_full_color_logo.svg.png",
+    width: 320,
+    height: 212,
+ },
+ {
+  src: "../../../public/tec/Bootstrap_logo.svg",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/Html_Css.jpeg",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/Js.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/Laravel.svg.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/Logo-csharp.webp",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/MariaDB_Logo.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/Microsoft_.NET_logo.svg.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/Microsoft_Azure.svg.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/PHP-logo.svg.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/Rabbit.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/React_logo.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/SqlServer.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/Symfony.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/tailwindcss.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/Typescript_logosvg.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/ubuntu.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/VB.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/Vite.svg",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/Windev.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/wserver.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/Xamarin-logo.svg.png",
+  width: 320,
+  height: 212,
+},
+{
+  src: "../../../public/tec/yarn.png",
+  width: 320,
+  height: 212,
+},
+];
+
 const Tecnologys = () => {
-    return <h1 className="text-yellow-600">Tecnologias</h1>;
+    return <Gallery images={images} />;
   };
   
   export default Tecnologys;
