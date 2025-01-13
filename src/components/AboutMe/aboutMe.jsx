7d26eba6fbc0292dd447bf6528fb5401761773ad
Alan <Alan@DESKTOP-D6S1PTH>
2025-01-12 23:06:16 -0600

    AP-12012025-cambio de about me

--- src/components/AboutMe/aboutMe.jsx
+++ src/components/AboutMe/aboutMe.jsx
@@ -3,17 +3,14 @@ const aboutMe = () => {
         <div className="text-center">
       <h1 className="text-7xl font-bold">Hola, soy Alan Parra</h1>
       <h2 className="text-2xl text-center pt-5 pb-5 align-text-top">
-        soy desarrollador back-end y tengo más de 6 años de experiencia 
-        desarrollando principalmente en PHP y C#.
+        soy desarrollador full stack y tengo más de 9 años de experiencia 
+        desarrollando principalmente en .Net y php.
       </h2>
-      <h1 className="text-2xl font-bold mt-4 text-left pl-6">Mención honorífica a otras tecnologías que uso:</h1>
+      <h1 className="text-2xl font-bold mt-4 text-left pl-6">Me especializo en desarrollos de sistemas dinamicos y de alta disponibilidad</h1>
       <ul className="list-disc text-lg mt-2 text-left pl-12">
-        <li>Javascript</li>
-        <li>Windev</li>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>TypeScript</li>
-        <li>Visual Basic</li>
+        <li>Software seguro</li>
+        <li>Diseño de base de datos</li>
+        <li>Metodologias aguiles (XP y RAD)</li>
       </ul>
     </div>
     );
