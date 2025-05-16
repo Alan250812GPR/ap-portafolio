a4aa11af1a6eddff81d97c62d3637128fa4ab7d9
Alan <Alan@DESKTOP-D6S1PTH>
2025-05-16 02:03:50 -0600

    AP-16052025-Fix Phone

--- src/components/Budget/Budget.jsx
+++ src/components/Budget/Budget.jsx
@@ -161,9 +161,7 @@ const Budget = () => {
             <div className="grid md:grid-cols-2 md:gap-6">
                 <div className="relative z-0 w-full mb-5 group">
                     <input
-                        type="tel"
-                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                        name="floating_phone"
+                        type="text"
                         id="PhoneCustomer" // Usa el nombre de la columna de Supabase como ID
                         className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         placeholder=" "
