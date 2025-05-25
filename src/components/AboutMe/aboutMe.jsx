ff7c361f2d38776b797c1955e117282dbde21ad4
Alan <Alan@DESKTOP-D6S1PTH>
2025-05-24 23:36:08 -0600

    AP-24052025-changes on about me

--- src/components/AboutMe/aboutMe.jsx
+++ src/components/AboutMe/aboutMe.jsx
@@ -3,13 +3,14 @@ const aboutMe = () => {
         <div className="text-center">
       <h1 className="text-7xl font-bold">Hola, soy Alan Parra</h1>
       <h2 className="text-2xl text-center pt-5 pb-5 align-text-top">
-        soy desarrollador full stack y tengo más de 9 años de experiencia 
+        soy desarrollador de software y tengo más de 9 años de experiencia
         desarrollando principalmente en .Net y php.
       </h2>
       <h1 className="text-2xl font-bold mt-4 text-left pl-6">Me especializo en desarrollos de sistemas dinamicos y de alta disponibilidad</h1>
       <ul className="list-disc text-lg mt-2 text-left pl-12">
         <li>Software seguro</li>
         <li>Diseño de base de datos</li>
+        <li>Arquitectura de Software RailWay</li>
         <li>Metodologias aguiles (XP y RAD)</li>
       </ul>
     </div>
