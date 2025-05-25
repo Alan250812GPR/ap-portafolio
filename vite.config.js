0fbfadb7f3a750746ae965d6345c46a585dce1dd
Alan <Alan@DESKTOP-D6S1PTH>
2025-05-24 19:00:52 -0600

    AP-16052025-Ofuscator

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { config } from 'dotenv';
+import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
 
 config();
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), // Tu configuración original de React
+    obfuscatorPlugin({
+      options: {
+        debugProtection: true,
+        // Aquí puedes añadir más opciones de ofuscación según tus necesidades
+        // Consulta la documentación de javascript-obfuscator para más detalles:
+        // https://github.com/javascript-obfuscator/javascript-obfuscator
+      },
+    }),
+  ],
   define: {
+    // Si necesitas la definición de process.env, asegúrate de mantenerla aquí
     'process.env': process.env
   }
-})
+});
